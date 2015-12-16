76c8a00edccee10565ff37f5ed438cc249d2197f
cfehlig <cassandrafehlig10@gmail.com>
2015-12-16 08:48:34 -0700

    Created FirstRobot project

---
+++ src/bot/controller/CassandraBotRunner.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package bot.controller;
+
+/**
+ * @author cfeh7454
+ *
+ */
+public class CassandraBotRunner
+{
+
+	/**
+	 * @param args unused in the Robot
+	 */
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+
+	}
+
+}
