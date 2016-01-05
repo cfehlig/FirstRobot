25f7d8661b892f8d7caf48ab3d5b46cb089cd59f
cfehlig <cassandrafehlig10@gmail.com>
2016-01-05 09:55:45 -0700

    added to EV3

--- src/bot/model/EV3Bot.java
+++ src/bot/model/EV3Bot.java
@@ -1,11 +1,39 @@
 package bot.model;
 
 import lejos.hardware.lcd.LCD;
+import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 
 public class EV3Bot
 {
-	public void driveRoom()
+	private String botMessage;
+	private int xPosition;
+	private int yPosition;
+	private long waitTime;
+	
+	private MovePilot botPilot;
+	
+	public EV3Bot()
+	{
+		this.botMessage = "Cassandra codes cassandraBot";
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime= 4000;
+		
+		setupPilot();
+		displayMessage();
+	}
+	
+	private void setupPilot
+	{
+		
+	}
+	
+	private void displayMessage(String message)
+	{
+		LCD.drawString(message,  xP);
+	}
+public void driveRoom()
 	{
 		
 	}
